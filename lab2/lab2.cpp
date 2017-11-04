7797d9c814f3e1f63a0cb5776575946d8595af99
kn37zh <kn37zh@gmail.com>
2017-11-04 14:32:44 +0200

    Console commands file or input

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -1,37 +1,52 @@
 #include <iostream>
 #include <fstream>
 #include <math.h>
+#include <string>
 
 int main() {
 	using namespace std;
 	int n;
 	double xmin, xmax, dx;
-	cout << "Enter integer n: ";
-	cin >> n;
+	string file("file");
+	string console("console");
+	string inputType;
+	cout << "What type of input do you prefer(file/console)?" << endl;
+	cin >> inputType;
+	if (inputType == file) {
 
-	if (n >= 3) {
-		cout << "Enter xMin, xMax, dx ";
-		cin >> xmin >> xmax >> dx;
+	}
+	else if (inputType == console) {
+	
+		cout << "Enter integer n: ";
+		cin >> n;
 
-		if (xmin < xmax && dx > 0) {
-			for (dx; xmin <= xmax; xmin += dx) {
-				double y = 0;
+		if (n >= 3) {
+			cout << "Enter xMin, xMax, dx ";
+			cin >> xmin >> xmax >> dx;
 
-				if (xmin >= 0) {
-					for (int i = 1; i <= (n - 2); i++) {
-						y += (i - xmin);
-					}
-				}
-				else {
-					for (double i = 0; i < n; i++) {
-						float prod = 1;
-						for (int j = i; j <= (n - 1); j++) {
-							prod *= (pow(i, 2) + j);
+			if (xmin < xmax && dx > 0) {
+				for (dx; xmin <= xmax; xmin += dx) {
+					double y = 0;
+
+					if (xmin >= 0) {
+						for (int i = 1; i <= (n - 2); i++) {
+							y += (i - xmin);
 						}
-						y += prod;
 					}
+					else {
+						for (double i = 0; i < n; i++) {
+							float prod = 1;
+							for (int j = i; j <= (n - 1); j++) {
+								prod *= (pow(i, 2) + j);
+							}
+							y += prod;
+						}
+					}
+					cout << "x = " << xmin << " " << "y= " << y << endl;
 				}
-				cout << "x = " << xmin << " " << "y= " << y << endl;
+			}
+			else {
+				cout << "Error" << endl;
 			}
 		}
 		else {
@@ -39,8 +54,9 @@ int main() {
 		}
 	}
 	else {
-		cout << "Error" << endl;
+		cout << "Error. Enter the right comand" << endl;
 	}
+
 	system("pause");
 	return 0;
 }
